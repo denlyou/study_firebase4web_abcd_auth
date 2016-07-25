955cec35a3c1e21add9453e8ff65f6b877952284
denlyou <denlyou85@gmail.com>
2016-07-25 13:23:48 +0900

    first init

---
+++ public/fireinit.js
@@ -0,0 +1,7 @@
+var config = {
+  apiKey: "AIzaSyC0VMya-6QGWcvKf5DE93U1V82Zp_yb9_4",
+  authDomain: "hellofirebase4web.firebaseapp.com",
+  databaseURL: "https://hellofirebase4web.firebaseio.com",
+  storageBucket: "hellofirebase4web.appspot.com",
+};
+firebase.initializeApp(config);
