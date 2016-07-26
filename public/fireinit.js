07025a5a11c528a81d8e5a7605bc1e14a5c64652
denlyou <denlyou85@gmail.com>
2016-07-27 01:06:48 +0900

    example update

--- public/fireinit.js
+++ public/fireinit.js
@@ -1,7 +1,9 @@
+// Initialize Firebase
+// TODO: Replace with your project's customized code snippet
 var config = {
-  apiKey: "AIzaSyC0VMya-6QGWcvKf5DE93U1V82Zp_yb9_4",
-  authDomain: "hellofirebase4web.firebaseapp.com",
-  databaseURL: "https://hellofirebase4web.firebaseio.com",
-  storageBucket: "hellofirebase4web.appspot.com",
+  apiKey: "apiKey",
+  authDomain: "projectId.firebaseapp.com",
+  databaseURL: "https://databaseName.firebaseio.com",
+  storageBucket: "bucket.appspot.com",
 };
 firebase.initializeApp(config);
